907234daaec562425bf5966afd0407f56949139e
Saulo Gomes <saulossg@gmail.com>
2022-11-29 23:10:03 -0300

    Config the file global.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
 
 import styled from 'styled-components';
 
 const Title = styled.h1`
   font-size: 30px;
-  color: blue;
+  color: red;
 `
 
 function App() {
-  return <Title>Hello World</Title>
+  return <Title>Hello World Saulo</Title>
 }
 
 export default App;
